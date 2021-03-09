d81ebe0fcf81faf6735f50bffab0e8029f7d0b35
Shay.Gazit <Shay.Gazit@verint.com>
2021-03-09 19:32:30 +0200

    initialize

---
+++ src/main/java/com/project/recommendationsystem/RecommendationsystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.recommendationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendationsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/recommendationsystem/RecommendationsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.recommendationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
